15825b6c1237fa361e607e965da88b36a4ea68d3
Ian Macalinao <me@ian.pw>
2015-09-05 04:22:55 -0400

    Add mongodb info

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'sinatra', '~> 1.4.6'
 gem 'mongo', '~> 2.0'
+gem 'dotenv', '~> 2.0.2'
--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
+require "dotenv"
+Dotenv.load
+
 require "sinatra"
+require "mongo"
+
+mongo = Mongo::Client.new([ENV["MONGODB_HOST"]], database: ENV["MONGODB_DB"])
 
 get "/" do
   "Hello, world!"
