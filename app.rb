e42c79d34536c533a410787fa29e9263a1c83ad2
Ian Macalinao <me@ian.pw>
2015-09-05 04:31:07 -0400

    Add GET /pois

--- app.rb
+++ app.rb
@@ -3,9 +3,18 @@ Dotenv.load
 
 require "sinatra"
 require "mongo"
+require "json"
 
-mongo = Mongo::Client.new([ENV["MONGODB_HOST"]], database: ENV["MONGODB_DB"])
+db = Mongo::Client.new([ENV["MONGODB_HOST"]], database: ENV["MONGODB_DB"])
 
 get "/" do
   "Hello, world!"
+end
+
+get "/pois" do
+  pois = []
+  db[:pois].find.each do |poi|
+    pois << poi
+  end
+  pois.to_json
 end
