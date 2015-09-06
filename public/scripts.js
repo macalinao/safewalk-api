535ca6ef43364dcf01c6053b61b320244bd6bca1
Ian Macalinao <me@ian.pw>
2015-09-06 07:26:28 -0400

    center on philly

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,10 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {
+      lat: 39.9306863,
+      lng: -75.1660835
+    },
+    zoom: 14
   });
 }
